77a46b49ea55d5e634b5c313fe2d1bba493f4391
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-04-05 16:22:00 +0530

    toco: Rebrand to surya
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- lineage_surya.mk
+++ lineage_surya.mk
@@ -8,16 +8,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from toco device
-$(call inherit-product, device/xiaomi/toco/device.mk)
+# Inherit from surya device
+$(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_toco
-PRODUCT_DEVICE := toco
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Mi Note 10 Lite
+PRODUCT_NAME := lineage_surya
+PRODUCT_DEVICE := surya
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO X3
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

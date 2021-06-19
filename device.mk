a49720da15d805c57ba4f6c054c44bd8f2ee586d
Astile97 <zilliondollarkuruvi@gmail.com>
2021-06-19 09:10:18 +0530

    surya: overlay: cleanup * Lineage >> AOSP * Remove unnecessary ones

--- device.mk
+++ device.mk
@@ -113,7 +113,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_PACKAGES += \
     KarnaFrameworks \
